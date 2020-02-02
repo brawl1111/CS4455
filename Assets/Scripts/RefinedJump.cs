2e413fec42870e58509b9a08bd32cde8f31aa34c
brawl1111 <tonywunj@yahoo.com>
2020-02-02 16:50:10 -0500

    Tweaked jumping to be less floaty. Also you can jump higher when you hold down the space bar longer.

---
+++ Assets/Scripts/RefinedJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefinedJump : MonoBehaviour
+{
+ 
+	public float fallMultiplier = 2.5f;
+	public float lowJumpMultiplier = 2f;
+
+	private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 		if (rb.velocity.y < 0)
+ 		{
+ 			rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1);
+ 		}
+ 		else if (rb.velocity.y > 0 && !Input.GetKey("space"))
+ 		{
+ 			rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1);
+ 		}
+    }
+}
