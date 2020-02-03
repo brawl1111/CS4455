c732a3a34ba907f36c8a9d00142e566df3a122f2
brawl1111 <tonywunj@yahoo.com>
2020-02-02 20:44:29 -0500

    Can spin to kill enemies.

---
+++ Assets/Scripts/EnemyKilled.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKilled : MonoBehaviour
+{
+
+	void Start() {
+		Debug.Log("start Hurtbox");
+	}
+    void OnTriggerEnter(Collider c)
+    {
+    	Debug.Log(c.gameObject);
+    	if (c.gameObject.CompareTag("Enemy"))
+    	{
+    		Debug.Log("detect enemy");
+    		c.gameObject.SetActive(false);
+    	}
+    }
+}
--- Assets/Scripts/SpinAttack.cs
+++ Assets/Scripts/SpinAttack.cs
@@ -7,6 +7,8 @@ public class SpinAttack : MonoBehaviour
 
 	private Animator anim;
 	private bool isSpinning;
+	private Transform hurtbox;
+	//private bool doneSpinning;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,10 @@ public class SpinAttack : MonoBehaviour
 
         if (anim == null)
             Debug.Log("Animator could not be found");
+
+        hurtbox = transform.GetChild(0);
+        if (hurtbox == null)
+            Debug.Log("BoxCollider could not be found");
     }
 
     // Update is called once per frame
@@ -21,21 +27,35 @@ public class SpinAttack : MonoBehaviour
     {
         if (Input.GetKeyDown("p") && !isSpinning)
         {
+        	anim.SetBool("doneSpinning", false);
         	anim.SetTrigger("spinTrigger");
         	isSpinning = true;
+        	hurtbox.gameObject.SetActive(true);
+        	StartCoroutine(disableBoxCollider());
         }
+    }
+
+    public bool getIsSpinning()
+    {
+    	return isSpinning;
     }
 
     public void startCooldown() {
     	StartCoroutine(Cooldown());
-    	//anim.SetTrigger("spinTrigger");
-    	//Debug.Log("coolddown over");
+
+    }
+
+    public void setDoneSpinning() {
+    	anim.SetBool("doneSpinning", true);
+    }
+
+    IEnumerator disableBoxCollider() {
+    	yield return new WaitForSeconds(0.7f);
+    	hurtbox.gameObject.SetActive(false);
     }
 
     IEnumerator Cooldown() {
     	yield return new WaitForSeconds(1.5f);
-    	//Debug.Log("hello");
     	isSpinning = false;
-    	//anim.SetTrigger("spinTrigger");
     }
 }
