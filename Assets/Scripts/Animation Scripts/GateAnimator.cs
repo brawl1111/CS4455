68a467df03f7a347bf211f3a2207a3eca9fd712c
Joey Bishop <jbishop47@gatech.edu>
2020-02-12 09:34:27 -0500

    Gate and Bug Fixes
    Adds the gate at the end of Section 1, with complete animation, script, and collision detection.
    Fixes character movement bug.

---
+++ Assets/Scripts/Animation Scripts/GateAnimator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateAnimator : MonoBehaviour
+{
+
+    private Animation anim;
+    private bool swinging = false;
+    private bool unswinging = false;
+    private bool inSwing = false;
+
+    void Start()
+    {
+        GameObject gate = gameObject.transform.Find("Gate").gameObject;
+
+        if (gate)
+        {
+            anim = gate.GetComponent<Animation>();
+        }
+    }
+
+    void Update()
+    {
+        if (anim.isPlaying)
+        {
+            return;
+        }
+        if (!anim.isPlaying && swinging)
+        {
+            anim.Play("Gate Swing");
+            inSwing = true;
+        }
+        if (!anim.isPlaying && unswinging)
+        {
+            anim.Play("Gate Unswing");
+            inSwing = false;
+        }
+        if (inSwing)
+        {
+            swinging = false;
+        }
+        else
+        {
+            unswinging = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.attachedRigidbody && !inSwing)
+        {
+            swinging = true;
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.attachedRigidbody && inSwing)
+        {
+            unswinging = true;
+        }
+    }
+}
