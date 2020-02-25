8c317952f4a3fdbba7a6d30e434a1462afa2506e
Joey Bishop <jbishop47@gatech.edu>
2020-02-25 17:20:17 -0500

    Knockable Tree
    Provides a knockable tree prefab.
    KT prefab cannot be pushed over by player.
    KT prefab will start an animation and fall upon spin collision by player.

---
+++ Assets/Scripts/TreeScrpit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScrpit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Gets the player object and checks if they are currently spinning
+        GameObject collider = collision.gameObject;
+        if (collider.gameObject == GameObject.Find("PlayerContainer"))
+        {
+            GameObject player = collider.gameObject.transform.GetChild(1).gameObject;
+            if (player == GameObject.Find("Player") && player.GetComponent<SpinAttack>().getIsSpinning())
+            {
+                // gameObject.GetComponent<Rigidbody>().mass = 25.0f;
+                gameObject.GetComponent<Animation>().Play("Tree Fall");
+            }
+        }
+    }
+}
