3f22f69a9c2570e07b888256a853ef5888e32e0f
Joey Bishop <jbishop47@gatech.edu>
2020-02-27 18:27:29 -0500

    Code documentation

--- Assets/Scripts/TreeScrpit.cs
+++ Assets/Scripts/TreeScrpit.cs
@@ -19,13 +19,13 @@ public class TreeScrpit : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         // Gets the player object and checks if they are currently spinning
+        // If so, we're gonna start the falling animation
         GameObject collider = collision.gameObject;
         if (collider.gameObject == GameObject.Find("PlayerContainer"))
         {
             GameObject player = collider.gameObject.transform.GetChild(1).gameObject;
             if (player == GameObject.Find("Player") && player.GetComponent<SpinAttack>().getIsSpinning())
             {
-                // gameObject.GetComponent<Rigidbody>().mass = 25.0f;
                 gameObject.GetComponent<Animation>().Play("Tree Fall");
             }
         }
