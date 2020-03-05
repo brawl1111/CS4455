d82b8eb9959416df3355a18dbb1486d788dad10c
Corey W <wencorey0@gmail.com>
2020-03-05 18:13:26 -0500

    platform puzzle added

---
+++ Assets/Scripts/TurnTile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTile : MonoBehaviour
+{
+
+    public GameObject bigTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hurtbox"))
+        {
+            if (Vector3.Distance(transform.position, other.transform.position) < 2)
+            {
+                transform.Rotate(0, 90, 0);
+                bigTile.transform.Rotate(0, 90, 0);
+                //bigTile.GetComponent<Animator>().Play("RotateTile90");
+                Debug.Log("TurnTile.cs: tile rotated");
+            }
+        }
+    }
+
+}
