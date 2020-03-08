ce45e8151601d99198c28f56b9c9f3080762bc85
Albert Xing <albertxing@hotmail.com>
2020-03-07 19:00:24 -0500

    pls no delet

--- Assets/SimpleSkeletonFree/Script/SkeletonSM.cs
+++ Assets/SimpleSkeletonFree/Script/SkeletonSM.cs
@@ -12,9 +12,7 @@ public class SkeletonSM : MonoBehaviour
         patrol_state,
         chase_state,
         attack_state,
-        defense_state,
-        flinch_state,
-        death_state
+        ready_state,
     };
 
     public AIState aiState;
@@ -27,8 +25,15 @@ public class SkeletonSM : MonoBehaviour
     private Transform target;
     private float timer;
 
+    private WaitForSeconds cooldown;
+    private WaitForSeconds idleTime;
+
+    private GameObject player;
+
     bool inPatrol;
     bool inRange;
+    bool ifSwapIdle;
+    bool ifSwapPatrol;
 
     // Start is called before the first frame update
     void Start()
@@ -37,31 +42,82 @@ public class SkeletonSM : MonoBehaviour
         skeletonAnim = GetComponent<Animator>();
         aiState = AIState.idle_state;
         timer = 0;
+        inRange = false;
+        ifSwapIdle = true;
+        ifSwapPatrol = true;
+        cooldown = new WaitForSeconds(5f);
+        idleTime = new WaitForSeconds(1f);
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        Debug.Log(distToPlayer);
         switch(aiState)
         {
             case AIState.idle_state:
-                StartCoroutine(SwapToIdle());
+                if (distToPlayer < 20.0f)
+                {
+                    skeletonAnim.SetBool("inPatrol", true);
+                    aiState = AIState.chase_state;
+                    break;
+                }
+                if (ifSwapIdle)
+                {
+                    StartCoroutine(SwapToPatrol());
+                    ifSwapIdle = false;
+                    ifSwapPatrol = true;
+                }
                 break;
             case AIState.patrol_state:
                 //timer += Time.deltaTime;
                 //timer >= wanderTimer &&
-
-                if (!skeletonNav.pathPending)
+                //Debug.Log("patrol");
+                if (distToPlayer < 20.0f)
                 {
-                    Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, NavMesh.AllAreas);
-                    skeletonNav.SetDestination(newPos);
-                    //timer = 0;
+                    skeletonAnim.SetBool("inPatrol", true);
+                    aiState = AIState.chase_state;
+                    break;
                 }
 
-                if (!inRange && skeletonNav.remainingDistance == 0)
+                Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, NavMesh.AllAreas);
+                skeletonNav.SetDestination(newPos);
+                //timer = 0;
+
+                if (!inRange && !skeletonNav.pathPending && ifSwapPatrol)
                 {
-                    aiState = AIState.idle_state;
-                    skeletonAnim.SetBool("inPatrol", false);
+                    StartCoroutine(SwapToIdle());
+                    ifSwapIdle = true;
+                    ifSwapPatrol = false;
+                    //Debug.Log("swap");
+                }
+                break;
+            case AIState.chase_state:
+                Vector3 dirToPlayer = transform.position - player.transform.position;
+                Vector3 chasePos = transform.position - dirToPlayer;
+                skeletonNav.SetDestination(chasePos);
+                if (distToPlayer < 3.0f)
+                {
+                    aiState = AIState.ready_state;
+                    skeletonNav.stoppingDistance = 3.0f;
+                }
+                break;
+            case AIState.ready_state:
+                skeletonAnim.SetBool("inPatrol", false);
+                if (3.0f < distToPlayer && distToPlayer < 20.0f)
+                {
+                    aiState = AIState.chase_state;
+                    skeletonAnim.SetBool("inPatrol", true);
+                    break;
+                }
+                if (distToPlayer > 20.0f)
+                {
+                    inRange = false;
+                    aiState = AIState.patrol_state;
+                    skeletonAnim.SetBool("inPatrol", true);
+                    break;
                 }
                 break;
         }
@@ -69,11 +125,18 @@ public class SkeletonSM : MonoBehaviour
 
     }
 
-    public IEnumerator SwapToIdle()
+    public IEnumerator SwapToPatrol()
     {
-        yield return new WaitForSeconds(.5f);
+        yield return idleTime;
         aiState = AIState.patrol_state;
         skeletonAnim.SetBool("inPatrol", true);
+    }
+
+    public IEnumerator SwapToIdle()
+    {
+        yield return cooldown;
+        aiState = AIState.idle_state;
+        skeletonAnim.SetBool("inPatrol", false);
     }
 
     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
