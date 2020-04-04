4888ff1cb49513d7a46e7679ac4baa6699a07dda
Joey Bishop <jbishop47@gatech.edu>
2020-04-04 11:38:05 -0400

    Pushable Crates can only be pushed while grounded

---
+++ Assets/Scripts/PushableCrateScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableCrateScript : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private CharacterMovement playerMvt;
+    private int massPushable = 250;
+    private int massHeavy = 10000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        playerMvt = GameObject.FindWithTag("Player").GetComponent<CharacterMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerMvt.isGroundedCheck)
+        {
+            rigidbody.mass = massPushable;
+        }
+        else
+        {
+            rigidbody.mass = massHeavy;
+        }
+    }
+}
