747021b528d48a364c5fd0ee63ec6a5bb8e72dd1
brawl1111 <tonywunj@yahoo.com>
2020-04-06 14:47:49 -0400

    moved apples to prefab folder, made apples restore health, worked on section3/4

---
+++ Assets/Scripts/AppleRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleRotate : MonoBehaviour
+{
+	public float rotateSpeed = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, rotateSpeed);
+    }
+}
---
+++ Assets/Scripts/Player Scripts/AppleRestoreHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleRestoreHealth : MonoBehaviour
+{
+	void Start()
+	{
+		gameObject.SetActive(true);
+	}
+    void OnTriggerEnter(Collider col)
+    {
+    	if (col.gameObject.CompareTag("Player"))
+    	{
+    		HealthManager.Instance.AddHealth(1);
+    		gameObject.SetActive(false);
+    	}
+    }
+}
