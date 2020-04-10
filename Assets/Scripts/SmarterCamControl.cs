cb495a108e70577a95ef86d374b1caad8922c0df
brawl1111 <tonywunj@yahoo.com>
2020-04-10 11:39:06 -0400

    Fixed camera

--- Assets/Scripts/SmarterCamControl.cs
+++ Assets/Scripts/SmarterCamControl.cs
@@ -10,6 +10,7 @@ public class SmarterCamControl : MonoBehaviour
 	public Vector3 followPOS;
 	public float topClampAngle = 60.0f;
 	public float bottomClampAngle = 40.0f;
+    public float mouseSensitivity = 20.0f;
 	public float inputSensitivity = 150.0f;
 	public float joystickSensitivity = 2.5f;
 	public GameObject camera;
@@ -21,10 +22,20 @@ public class SmarterCamControl : MonoBehaviour
 	public float smoothY;
 	private float mouseX;
 	private float mouseY;
+    private float joystickX;
+    private float joystickY;
 	private float finalInputX;
 	private float finalInputZ;
 	private float rotY = 0.0f;
 	private float rotX = 0.0f; 
+
+
+    enum CamControlState
+    {
+        mouseControl,
+        joystickControl
+    };
+    CamControlState state;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +43,7 @@ public class SmarterCamControl : MonoBehaviour
         Vector3 rot = transform.localRotation.eulerAngles;
         rotY = rot.y;
         rotX = rot.x;
+        state = CamControlState.joystickControl;
         //Cursor.lockState = CursorLockMode.Locked;
         //Cursor.visible = false;
     }
@@ -39,15 +51,57 @@ public class SmarterCamControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Joystick X") * joystickSensitivity;
-        float inputY = Input.GetAxis("Joystick Y") * joystickSensitivity;
+        switch(state)
+        {
+            case CamControlState.mouseControl:
+                mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+                mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+                rotY += mouseX * inputSensitivity * Time.deltaTime;
+                rotX += mouseY * inputSensitivity * Time.deltaTime;
+
+                rotX = Mathf.Clamp(rotX, -bottomClampAngle, topClampAngle);
+
+                Quaternion localRot1 = Quaternion.Euler(rotX, rotY, 0.0f);
+                transform.rotation = localRot1;
+                if (Input.GetMouseButtonUp(1))
+                {
+                    joystickX = mouseX;
+                    joystickY = mouseY;
+                    state = CamControlState.joystickControl;
+                }
+            break;
+
+            case CamControlState.joystickControl:
+                joystickX = Input.GetAxis("Joystick X") * joystickSensitivity;
+                joystickY = Input.GetAxis("Joystick Y") * joystickSensitivity;
+                rotY += joystickX * inputSensitivity * Time.deltaTime;
+                rotX += joystickY * inputSensitivity * Time.deltaTime;
+
+                rotX = Mathf.Clamp(rotX, -bottomClampAngle, topClampAngle);
+
+                Quaternion localRot = Quaternion.Euler(rotX, rotY, 0.0f);
+                transform.rotation = localRot;
+                if (Input.GetMouseButton(1))
+                {
+                    mouseX = joystickX;
+                    mouseY = joystickY;
+                    state = CamControlState.mouseControl;
+                }
+            break;
+
+        }
+
+
+
+        /*joystickX = Input.GetAxis("Joystick X") * joystickSensitivity;
+        joystickY = Input.GetAxis("Joystick Y") * joystickSensitivity;
         if (Input.GetMouseButton(1))
         {
             mouseX = Input.GetAxis("Mouse X");
             mouseY = Input.GetAxis("Mouse Y");
         }
-        finalInputX = inputX + mouseX;
-        finalInputZ = inputY + mouseY;
+        finalInputX = joystickX + mouseX;
+        finalInputZ = joystickY + mouseY;
 
         rotY += finalInputX * inputSensitivity * Time.deltaTime;
         rotX += finalInputZ * inputSensitivity * Time.deltaTime;
@@ -55,7 +109,7 @@ public class SmarterCamControl : MonoBehaviour
         rotX = Mathf.Clamp(rotX, -bottomClampAngle, topClampAngle);
 
         Quaternion localRot = Quaternion.Euler(rotX, rotY, 0.0f);
-        transform.rotation = localRot;
+        transform.rotation = localRot;*/
     }
 
     void LateUpdate()
