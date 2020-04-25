e02927fc2b26117c34bd6329a976171973aaede9
Joey Bishop <jbishop47@gatech.edu>
2020-04-24 22:45:22 -0400

    Adds flag animations when player reaches checkpoints

---
+++ Assets/Scripts/FlagController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    public GameObject flag;
+    private Animation flagRaise;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flagRaise = flag.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            flagRaise.Play();
+        }
+    }
+}
