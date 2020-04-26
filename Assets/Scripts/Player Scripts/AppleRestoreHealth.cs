64ca3da60f2b06f6570552fb045f9a8c411e1724
brawl1111 <tonywunj@yahoo.com>
2020-04-26 15:08:09 -0400

    Added heart particles when you eat an apple. Also removed some cacti to improve performance

--- Assets/Scripts/Player Scripts/AppleRestoreHealth.cs
+++ Assets/Scripts/Player Scripts/AppleRestoreHealth.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AppleRestoreHealth : MonoBehaviour
 {
 	public int healthRestored = 1;
+	public GameObject heartParticles;
+
 	void Start()
 	{
 		gameObject.SetActive(true);
@@ -14,6 +16,7 @@ public class AppleRestoreHealth : MonoBehaviour
     	if (col.gameObject.CompareTag("Player"))
     	{
     		HealthManager.Instance.AddHealth(healthRestored);
+    		Instantiate(heartParticles, transform.position, Quaternion.identity);
     		gameObject.SetActive(false);
     		EventManager.TriggerEvent<EatAppleSFXEvent, Vector3>(this.transform.position);
     	}
