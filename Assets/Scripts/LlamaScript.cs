e46cb324aac10eee028894021888a5d05de943bc
brawl1111 <tonywunj@yahoo.com>
2020-04-27 02:02:53 -0400

    Added particles to when you collect a llama

--- Assets/Scripts/LlamaScript.cs
+++ Assets/Scripts/LlamaScript.cs
@@ -6,7 +6,7 @@ public class LlamaScript : MonoBehaviour
 {
     // Which section of the game this llama is in
     //public int section;
-
+    public GameObject starParticles;
     private bool collected = false;
 
     void OnCollisionEnter(Collision collision)
@@ -20,6 +20,7 @@ public class LlamaScript : MonoBehaviour
             collected = true;
             EventManager.TriggerEvent<LlamaPickupSFXEvent, Vector3>(this.transform.position);
             Debug.Log("detected llama collision");
+            Instantiate(starParticles, transform.position, Quaternion.identity);
             this.gameObject.SetActive(false);
             LlamaCounter.Instance.IncrementLlamaCount();
         }
