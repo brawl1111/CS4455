827c676924dca73a51fc5cbf4a29b1a2456dabef
Albert Xing <albertxing@hotmail.com>
2020-04-27 12:00:55 -0400

    made tiles glow xd

---
+++ Assets/Scripts/GlowTiles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowTiles : MonoBehaviour
+{
+    public GameObject bigTile;
+    private Renderer tileRender;
+    private Renderer[] bigRender;
+
+    void Start()
+    {
+        tileRender = GetComponent<Renderer>();
+        bigRender = bigTile.GetComponentsInChildren<Renderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            tileRender.material.SetColor("_Color", Color.green);
+
+            foreach(Renderer rend in bigRender)
+            {
+                rend.material.SetColor("_Color", Color.green);
+            }              
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        tileRender.material.SetColor("_Color", Color.white);
+        tileRender.material.SetColor("_Emission", Color.black);
+
+        foreach (Renderer rend in bigRender)
+        {
+            rend.material.SetColor("_Color", Color.white);
+            rend.material.SetColor("_Emission", Color.black);
+        }
+    }
+}
