a3d022748f9b05e0e5838e69600c1ba193e6b6ea
brawl1111 <tonywunj@yahoo.com>
2020-04-28 20:29:30 -0400

    Princess will now sit on throne for a few seconds before ending the game

--- Assets/Scripts/ThroneController.cs
+++ Assets/Scripts/ThroneController.cs
@@ -18,6 +18,7 @@ public class ThroneController : MonoBehaviour
         if (c.CompareTag("Player"))
         {
             // SET THE WIN SCREEN
+            Debug.Log("starting sitting");
             StartCoroutine(SittingSequence());
             // GameObject.Find("GameWinCanvas").GetComponent<GameWinMenuToggle>().GameWinMenuOn();
         }
@@ -28,10 +29,17 @@ public class ThroneController : MonoBehaviour
     {
         player.GetComponent<CharacterMovement>().enabled = false;
         player.GetComponent<CharacterMovement>().SetAnimMovementToZero();
-        player.transform.forward = -1f * transform.forward;
+        player.GetComponent<Rigidbody>().isKinematic = true;
+        player.GetComponent<CapsuleCollider>().enabled = false;
+        anim.SetBool("isFalling", false);
         anim.SetTrigger("gameDone");
+        player.transform.forward = -1f * transform.forward;
+        player.transform.position = new Vector3(-19.7f, 231f, 41f);
+        // anim.MatchTarget(new Vector3(-19.7f, 232f, 41f), Quaternion.Euler(0f, -179f, 0f), AvatarTarget.Root,
+        //     new MatchTargetWeightMask(new Vector3(1f, 0f, 1f), 1f),
+        //     anim.GetCurrentAnimatorStateInfo(0).normalizedTime, 0.75f);
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(5f);
         GameObject.Find("GameWinCanvas").GetComponent<GameWinMenuToggle>().GameWinMenuOn();
     }
 }
