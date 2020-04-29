823c00101d3917d30966fd230ef5881718d21370
brawl1111 <tonywunj@yahoo.com>
2020-04-29 08:34:12 -0400

    added fade in to credits

--- Assets/Scripts/Event Scripts/GameToMenuScript.cs
+++ Assets/Scripts/Event Scripts/GameToMenuScript.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameToMenuScript : MonoBehaviour
 {
+    public Image black;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = black.GetComponent<Animator>();
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -20,6 +23,14 @@ public class GameToMenuScript : MonoBehaviour
     public void GoToMenu()
     {
         EventManager.TriggerEvent<ButtonClickEvent, Vector3>(transform.position);
+        Time.timeScale = 1f;
+        StartCoroutine(Fading());
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
         SceneManager.LoadScene("MainMenu");
     }
 }
